fcdcbbf16818d8103e595fd9f44476f77e2fa0be
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-07-22 15:28:02 +0200

    feat: first definition of domain actions

---
+++ milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/types/Actions.scala
@@ -0,0 +1,9 @@
+package dev.atedeg.mdm.milkplanning.types
+
+// TODO: raises an event
+def estimateQuintalsOfMilk(
+    milkOfThePreviousYear: QuintalsOfMilk,
+    milkNeededByProducts: QuintalsOfMilk,
+    currentStock: Stock,
+    stockedMilk: QuintalsOfMilk,
+): Unit = ???
