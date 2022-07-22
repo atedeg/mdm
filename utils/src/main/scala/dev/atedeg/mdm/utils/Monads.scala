4e31c2b93c27da0ab332e34af7f452ac2d383ee8
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-22 10:26:08 +0200

    chore(utils): add unless/when utility methods

--- utils/src/main/scala/dev/atedeg/mdm/utils/Monads.scala
+++ utils/src/main/scala/dev/atedeg/mdm/utils/Monads.scala
@@ -2,6 +2,7 @@ package dev.atedeg.mdm.utils
 
 import cats.data.NonEmptyList
 import cats.mtl.{Raise, Tell}
+import cats.Monad
 
 /**
  * Signals that a method could emit elements of a given type which are accumulated in a list.
@@ -29,3 +30,13 @@ def emit[M[_], E](e: E)(using T: Tell[M, List[E]]): M[Unit] = T.tell(List(e))
  * Raises an error of type `E` in a context `M[_]` where the computation can be aborted.
  */
 def raise[M[_], E, A](e: E)(using R: Raise[M, E]): M[A] = R.raise(e)
+
+/**
+ * `unless(cond)(a)` performs the monadic action `a` if the condition `cond` is false.
+ */
+def unless[M[_], A](cond: => Boolean)(action: => M[A])(using M: Monad[M]): M[Unit] = M.unlessA(cond)(action)
+
+/**
+ * `when(cond)(a)` performs the monadic action `a` if the condition `cond` holds true.
+ */
+def when[M[_], A](cond: => Boolean)(action: => M[A])(using M: Monad[M]): M[Unit] = M.whenA(cond)(action)
