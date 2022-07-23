18b0dd46d09a2af8c8a9d1ea403d2da5656d3b8c
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-23 18:33:02 +0200

    chore(utils): add more monads utility functions

--- utils/src/main/scala/dev/atedeg/mdm/utils/Monads.scala
+++ utils/src/main/scala/dev/atedeg/mdm/utils/Monads.scala
@@ -32,13 +32,6 @@ def emit[M[_], E](e: E)(using T: Tell[M, List[E]]): M[Unit] = T.tell(List(e))
  */
 def raise[M[_], E, A](e: E)(using R: Raise[M, E]): M[A] = R.raise(e)
 
-extension(condition: Boolean)
-  /**
-   * `cond.otherwiseRaise(err)` [[raise() raises]] the error `err` if the condition `cond` is true.
-   */
-  def otherwiseRaise[M[_], E](error: => E)(using R: Raise[M, E], M: Monad[M]): M[Boolean] =
-    unless[M, Boolean](condition)(raise(error)).map(_ => condition)
-
 /**
  * `unless(cond)(a)` performs the monadic action `a` if the condition `cond` is false.
  */
@@ -48,3 +41,29 @@ def unless[M[_], A](cond: => Boolean)(action: => M[A])(using M: Monad[M]): M[Uni
  * `when(cond)(a)` performs the monadic action `a` if the condition `cond` holds true.
  */
 def when[M[_], A](cond: => Boolean)(action: => M[A])(using M: Monad[M]): M[Unit] = M.whenA(cond)(action)
+
+extension(condition: Boolean)
+  /**
+   * `cond.otherwiseRaise(err)` [[raise() raises]] the error `err` if the condition `cond` is true.
+   */
+  def otherwiseRaise[M[_], E](error: => E)(using R: Raise[M, E], M: Monad[M]): M[Boolean] =
+    unless[M, Boolean](condition)(raise(error)).map(_ => condition)
+
+extension[M[_]: Monad, A] (ma: M[A])
+  /**
+   * `ma.thenReturn(b)` performs the monadic action `ma`, ignores its return value
+   * and then returns the value `b` in the context `M[_]`.
+   */
+  def thenReturn[B](b: B): M[B] = ma *> b.pure
+
+  /**
+   * `ma.andThen(mb)` performs the monadic action `ma`, ignores its return value
+   * and then performs the monadic action `mb`.
+   */
+  def andThen[B](mb: M[B]): M[B] = ma *> mb
+
+  /**
+   * `ma.void` performs the monadic action `ma` then discards its return value returning
+   * `Unit` in the context `M[_]`.
+   */
+  def void = ma thenReturn ()
