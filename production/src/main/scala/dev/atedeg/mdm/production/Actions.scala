a6687ea3ce0ad5f45bda2854307b8a2949c69a74
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-25 23:39:27 +0200

    feat: add first draft implementation of one of the actions

--- production/src/main/scala/dev/atedeg/mdm/production/Actions.scala
+++ production/src/main/scala/dev/atedeg/mdm/production/Actions.scala
@@ -1,7 +1,8 @@
 package dev.atedeg.mdm.production
 
 import cats.Monad
-import dev.atedeg.mdm.utils.{CanRaise, Emits}
+import cats.syntax.all.*
+import dev.atedeg.mdm.utils.*
 import OutgoingEvent.*
 
 // TODO: Does this action emits a "startProduction" event that is used by the machines? Or does it
@@ -13,7 +14,16 @@ import OutgoingEvent.*
  */
 def startProduction[M[_]: Monad: CanRaise[MissingRecipe]: Emits[IngredientUsed]]
   (recipeBook: RecipeBook)
-  (production: Production.ToStart): Production.InProgress = ???
+  (production: Production.ToStart)
+  : M[Production.InProgress] =
+    val typeToProduce = production.productToProduce.cheeseType
+    val gramsOfSingleUnit = production.productToProduce.weight
+    for {
+      recipe <- recipeBook(typeToProduce) ifMissingRaise MissingRecipe(typeToProduce)
+      quintalsToProduce = production.unitsToProduce * gramsOfSingleUnit//.toQuintals
+      neededIngredients = recipe.lines.map(l => (l.ingredient, l.quintalsNeeded * quintalsToProduce))
+      _ <- neededIngredients.forEachDo((i, w) => emit(IngredientUsed(i, w): IngredientUsed))
+    } yield Production.InProgress(production.ID, production.productToProduce, production.unitsToProduce)
 
 /**
  * Ends a [[Production.InProgress production]] by assigning it a [[LotNumber lot number]].
