dcfd6a48d3980274226e1dfa6a55c6d0fbbe44f8
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-07-26 22:07:27 +0200

    style: reformat files

--- milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/utils/TypesOps.scala
+++ milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/utils/TypesOps.scala
@@ -2,12 +2,29 @@ package dev.atedeg.mdm.milkplanning.utils
 
 import scala.annotation.targetName
 
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.{ NonNegative, Positive }
+import eu.timepit.refined.refineV
+
 import dev.atedeg.mdm.milkplanning.types.QuintalsOfMilk
+import dev.atedeg.mdm.utils.*
+import dev.atedeg.mdm.utils.{ NonNegativeDecimal, PositiveDecimal }
+import dev.atedeg.mdm.utils.given
 
 extension (qom1: QuintalsOfMilk)
 
   @targetName("plus")
-  def +(qom2: QuintalsOfMilk): QuintalsOfMilk = ???
+  def +(qom2: QuintalsOfMilk): QuintalsOfMilk = {
+    val result: Option[NonNegativeDecimal] = qom1.n.value + qom2.n.value
+    result match
+      case Some(value) => QuintalsOfMilk(value)
+      case _ => ??? // TODO: how can we handle this case?
+  }
 
   @targetName("minus")
-  def -(qom2: QuintalsOfMilk): Option[QuintalsOfMilk] = ???
+  def -(qom2: QuintalsOfMilk): QuintalsOfMilk = {
+    val result: Option[NonNegativeDecimal] = qom1.n.value - qom2.n.value
+    result match
+      case Some(value) => QuintalsOfMilk(value)
+      case _ => QuintalsOfMilk(0.0.nonNegativeDecimal)
+  }
