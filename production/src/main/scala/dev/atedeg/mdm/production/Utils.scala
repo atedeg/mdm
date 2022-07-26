e2d4349f7575b9130703a5d1e855ff3535a4ed13
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-26 12:29:26 +0200

    chore: add utility definitions

---
+++ production/src/main/scala/dev/atedeg/mdm/production/Utils.scala
@@ -0,0 +1,26 @@
+package dev.atedeg.mdm.production
+
+import scala.annotation.targetName
+
+type PositiveDouble = Double // TODO: get from utils
+final case class WeightInGrams(grams: PositiveDouble)
+final case class WeightInQuintals(quintals: PositiveDouble)
+
+extension (weight: WeightInGrams)
+  def map(f: PositiveDouble => PositiveDouble): WeightInGrams = WeightInGrams(f(weight.grams))
+  def toQuintals: WeightInQuintals = WeightInQuintals(weight.grams / 100_000)
+
+extension (weight: WeightInQuintals)
+  def map(f: PositiveDouble => PositiveDouble): WeightInQuintals = WeightInQuintals(f(weight.quintals))
+  def toGrams: WeightInGrams = WeightInGrams(weight.quintals * 100_000)
+  @targetName("multiply") def *(other: WeightInQuintals) = weight.map(_ * other.quintals)
+
+extension (q: Quantity)
+  @targetName("multiplyGrams") def *(weight: WeightInGrams): WeightInGrams = weight.map(_ * q)
+  @targetName("multiplyQuintals") def *(weight: WeightInQuintals): WeightInQuintals = weight.map(_ * q)
+
+extension (q: QuintalsOfIngredient)
+  def map(f: PositiveDouble => PositiveDouble): QuintalsOfIngredient =
+    QuintalsOfIngredient(q.quintals.map(f), q.ingredient)
+
+  def *(w: WeightInQuintals): QuintalsOfIngredient = QuintalsOfIngredient(w * q.quintals, q.ingredient)
