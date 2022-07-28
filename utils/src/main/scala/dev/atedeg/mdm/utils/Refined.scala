6f67f3cedcea5ee04e8a622ecaaa116c08115a57
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-07-28 14:26:01 +0200

    fix: rewrite code so that coverage is happy and no longer crashes misteriously

--- utils/src/main/scala/dev/atedeg/mdm/utils/Refined.scala
+++ utils/src/main/scala/dev/atedeg/mdm/utils/Refined.scala
@@ -2,7 +2,6 @@ package dev.atedeg.mdm.utils
 
 import scala.annotation.targetName
 
-import Coercion.*
 import cats.kernel.{ Eq, Order }
 import cats.syntax.all.*
 import eu.timepit.refined.api.{ Refined, Validate }
@@ -17,13 +16,6 @@ type NonNegativeNumber = Int Refined NonNegative
 type NonNegativeDecimal = Double Refined NonNegative
 private type ValidFor[N] = [P] =>> Validate[N, P]
 
-private object Coercion:
-
-  extension [A](a: A)
-
-    @SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
-    def coerceTo[P](using Validate[A, P]): A Refined P = refineV[P](a).toOption.get
-
 // `T Refined P` has an order relation if `T` has an order relation
 given refinedOrd[N: Order, P]: Order[N Refined P] with
   override def compare(x: N Refined P, y: N Refined P): Int = Order[N].compare(x.value, y.value)
@@ -33,18 +25,21 @@ given refinedEq[N: Eq, P]: Eq[N Refined P] with
   override def eqv(x: N Refined P, y: N Refined P): Boolean = Eq[N].eqv(x.value, y.value)
 
 // Instances for the various numeric ops
+@SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
+private def coerce[A, P](a: A)(using Validate[A, P]): A Refined P = refineV[P](a).toOption.get
+
 given refinedPlus[N, P <: Positive | NonNegative: ValidFor[N]](using Op: Plus[N]): Plus[N Refined P] with
-  override def plus(x: N Refined P, y: N Refined P): N Refined P = Op.plus(x.value, y.value).coerceTo[P]
+  override def plus(x: N Refined P, y: N Refined P): N Refined P = coerce(Op.plus(x.value, y.value))
 
 given refinedTimes[N, P <: Positive | NonNegative: ValidFor[N]](using Op: Times[N]): Times[N Refined P] with
-  override def times(x: N Refined P, y: N Refined P): N Refined P = Op.times(x.value, y.value).coerceTo[P]
+  override def times(x: N Refined P, y: N Refined P): N Refined P = coerce(Op.times(x.value, y.value))
 
 given refinedDiv[N: Numeric, P <: NonNegative: ValidFor[N]](using Op: Div[N]): Div[N Refined P] with
 
   override def div(x: N Refined P, y: N Refined P): N Refined P =
-    (if y.value > x.value then Numeric[N].zero else Op.div(x.value, y.value)).coerceTo[P]
+    coerce(if y.value > x.value then Numeric[N].zero else Op.div(x.value, y.value))
 
 given refinedMinus[N: Numeric, P <: NonNegative: ValidFor[N]](using Op: Minus[N]): Minus[N Refined P] with
 
   override def minus(x: N Refined P, y: N Refined P): N Refined P =
-    (if y.value > x.value then Numeric[N].zero else Op.minus(x.value, y.value)).coerceTo[P]
+    coerce(if y.value > x.value then Numeric[N].zero else Op.minus(x.value, y.value))
