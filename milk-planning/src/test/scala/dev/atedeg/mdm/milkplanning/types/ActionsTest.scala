8d5e538f8b3c0ed7500fdd83b173d9f2fc42cc93
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-07-28 14:25:56 +0200

    test: partial implementation of the test

--- milk-planning/src/test/scala/dev/atedeg/mdm/milkplanning/types/ActionsTest.scala
+++ milk-planning/src/test/scala/dev/atedeg/mdm/milkplanning/types/ActionsTest.scala
@@ -1,36 +1,55 @@
 package dev.atedeg.mdm.milkplanning.types
 
-import cats.data.Writer
+import java.time.LocalDateTime
+
+import cats.data.{ NonEmptyList, Writer }
 import org.scalatest.GivenWhenThen
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
 
 import dev.atedeg.mdm.milkplanning.dsl.*
 import dev.atedeg.mdm.milkplanning.types.OutgoingEvent.OrderMilk
+import dev.atedeg.mdm.milkplanning.types.Product.*
+import dev.atedeg.mdm.utils.*
 
-class ActionsTest extends AnyFeatureSpec with GivenWhenThen with Matchers {
+trait Fixture {
+
+  val recipeBook: RecipeBook = Map(
+    CheeseType.Squacquerone -> QuintalsOfMilk(1.3),
+    CheeseType.Casatella -> QuintalsOfMilk(1.3),
+    CheeseType.Ricotta -> QuintalsOfMilk(1.1),
+    CheeseType.Stracchino -> QuintalsOfMilk(1.4),
+    CheeseType.Caciotta -> QuintalsOfMilk(1.4),
+  )
+}
+
+class ActionsTest extends AnyFeatureSpec with GivenWhenThen with Matchers with Fixture {
 
   Feature("Estimate the quintals of milk needed for the following week") {
-//    Scenario("Simple estimation") {
-//      Given("A request for ordering milk")
-//      val qomPreviousYear = 4.0.quintalsOfMilk
-//      val qomNeededByProd = 3.5.quintalsOfMilk
-//      val currentStock: Stock = _ => 0.stockedQuantity
-//      val stockedMilk = 0.0.quintalsOfMilk
-//      When("the estimation is completed")
-//      val estimationMonad: Writer[List[OrderMilk], QuintalsOfMilk] = estimateQuintalsOfMilk(
-//        qomPreviousYear,
-//        qomNeededByProd,
-//        currentStock,
-//        stockedMilk,
-//      )
-//      Then("an event should be raised in order to place the order")
-//      val (events, qom) = estimationMonad.run
-//      events should have length 1
-//      events.headOption match {
-//        case Some(e) => e.n should equal(qom)
-//        case _ => fail("The events list must have at least one element")
-//      }
-//    }
+    Scenario("Raffaella wants to estimate the quintals of milk") {
+      Given("the quintals of milk of the previous year for the same period")
+      And("a list of products to be produced")
+      And("an empty stock")
+      And("no milk in stock")
+      val qomPreviousYear = QuintalsOfMilk(4.0)
+      val requestedProducts = NonEmptyList.of(
+        RequestedProduct(Squacquerone(100), Quantity(50), LocalDateTime.now()),
+      )
+      val currentStock: Stock = _ => StockedQuantity(0)
+      val stockedMilk = QuintalsOfMilk(0.0)
+      When("the estimation is ready to be computed")
+      val estimatorMonad: Writer[List[OrderMilk], QuintalsOfMilk] = estimateQuintalsOfMilk(
+        qomPreviousYear,
+        requestedProducts,
+        currentStock,
+        recipeBook,
+        stockedMilk,
+      )
+      val (events, estimation) = estimatorMonad.run
+      println(estimation)
+
+      Then("the result will be in the interval [..., ...]")
+
+    }
   }
 }
