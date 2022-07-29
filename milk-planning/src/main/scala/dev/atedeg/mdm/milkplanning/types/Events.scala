a2e01748f7a94c7d2117a44e10c7ca048f3690bb
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-07-29 18:13:13 +0200

    feat: define incoming and outgoing events

--- milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/types/Events.scala
+++ milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/types/Events.scala
@@ -1,11 +1,24 @@
 package dev.atedeg.mdm.milkplanning.types
 
+import cats.data.NonEmptyList
+
 import dev.atedeg.mdm.products.Product
 
+/**
+ * Events managed by the bounded context.
+ */
 enum IncomingEvent:
-  case ProductRemovedFromStock(product: Product)
-  case ProductAddedToStock(product: Product)
-  case RestockedMilk(quintalsOfMilk: QuintalsOfMilk)
+  /**
+   * Event representing an order placed used to estimate the [[QuintalsOfMilk quintals of milk]] to be ordered.
+   */
+  case ReceivedOrder(products: NonEmptyList[RequestedProduct])
 
+/**
+ * Events sent by the bounded context.
+ */
 enum OutgoingEvent:
+  /**
+   * Event to order the [[QuintalsOfMilk quintals of milk]] needed for the next week.
+   * This event is emitted every week on saturday.
+   */
   case OrderMilk(n: QuintalsOfMilk)
