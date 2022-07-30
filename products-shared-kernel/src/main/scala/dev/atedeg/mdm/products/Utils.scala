8a025ce967554f36164a76128a08d667fb32b6a9
ndido98 <ndido98@gmail.com>
2022-07-30 16:47:52 +0200

    refactor: rewrite labelProduct for better clarity

--- products-shared-kernel/src/main/scala/dev/atedeg/mdm/products/Utils.scala
+++ products-shared-kernel/src/main/scala/dev/atedeg/mdm/products/Utils.scala
@@ -3,6 +3,7 @@ package dev.atedeg.mdm.products
 import scala.compiletime.*
 
 import cats.data.NonEmptyList
+import cats.kernel.Order
 import dev.atedeg.mdm.utils.{PositiveNumber, coerce}
 import eu.timepit.refined.predicates.all.Positive
 
@@ -28,3 +29,30 @@ inline private def checkInt[T](inline n: T): Int = inline erasedValue[T] match
   case _ => compiletime.error(codeOf(n) + " is not an int")
 
 private[products] def toGrams(n: Int): Grams = Grams(coerce[Int, Positive](n))
+
+given Order[Grams] with
+  def compare(x: Grams, y: Grams): Int = Order[Int].compare(x.n.value, y.n.value)
+
+extension (cheeseType: CheeseType)
+
+  /**
+   * Creates a [[Product product]] with the given [[CheeseType cheese type]] if it can find a
+   * weight amongst the allowed ones that matches the given predicate.
+   */
+  def withWeight(predicate: Int => Boolean): Option[Product] =
+    // Somehow Scala does not understand that a function (A => B) could also be treated as a function
+    // [a <: A] => a => B and won't compile unless I explicitly convert the function myself...
+    def p = [t <: Int] => (n: t) => predicate(n)
+    cheeseType match
+      case CheeseType.Squacquerone => allSquacqueroneWeights.find(p(_)).map(Product.Squacquerone(_))
+      case CheeseType.Casatella => allCasatellaWeights.find(p(_)).map(Product.Casatella(_))
+      case CheeseType.Ricotta => allRicottaWeights.find(p(_)).map(Product.Ricotta(_))
+      case CheeseType.Stracchino => allStracchinoWeights.find(p(_)).map(Product.Stracchino(_))
+      case CheeseType.Caciotta => allCaciottaWeights.find(p(_)).map(Product.Caciotta(_))
+
+  def allowedWeights: NonEmptyList[Grams] = cheeseType match
+    case CheeseType.Squacquerone => allSquacqueroneWeights.map(toGrams)
+    case CheeseType.Casatella => allCasatellaWeights.map(toGrams)
+    case CheeseType.Ricotta => allRicottaWeights.map(toGrams)
+    case CheeseType.Stracchino => allStracchinoWeights.map(toGrams)
+    case CheeseType.Caciotta => allCaciottaWeights.map(toGrams)
