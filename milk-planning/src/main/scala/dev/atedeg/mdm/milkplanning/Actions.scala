dd470313e321f6bdd0a42253e30b88c65146fa4f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-07-30 11:27:38 +0200

    refactor: use implicit conversion between refined types and improve method docs

--- milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/Actions.scala
+++ milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/Actions.scala
@@ -1,10 +1,10 @@
-package dev.atedeg.mdm.milkplanning.types
+package dev.atedeg.mdm.milkplanning
 
 import cats.Monad
 import cats.data.NonEmptyList
 import cats.syntax.all.*
 
-import dev.atedeg.mdm.milkplanning.types.OutgoingEvent.*
+import dev.atedeg.mdm.milkplanning.OutgoingEvent.*
 import dev.atedeg.mdm.milkplanning.utils.QuintalsOfMilkOps.*
 import dev.atedeg.mdm.milkplanning.utils.QuintalsOfMilkOps.given
 import dev.atedeg.mdm.milkplanning.utils.given
@@ -14,7 +14,10 @@ import dev.atedeg.mdm.utils.given
 import dev.atedeg.mdm.utils.monads.{ emit, thenReturn, when, Emits }
 
 /**
- * .
+ * Estimate the amount of milk needed for the following week's production.
+ * The estimate takes into account the milk processed in the same week last year,
+ * the products ordered for the following week, the current [[Stock stock]] and the [[QuintalsOfMilk quintals of milk]]
+ * currently in stock and return the [[QuintalsOfMilk quintals of milk]] needed for the following week.
  */
 def estimateQuintalsOfMilk[M[_]: Emits[OrderMilk]: Monad](
     milkOfPreviousYear: QuintalsOfMilk,
@@ -43,10 +46,10 @@ private def milkNeededForProduct(
     recipeBook: RecipeBook,
 ): QuintalsOfMilk =
   val RequestedProduct(p @ Product(cheeseType, weight), quantity, _) = product
-  val unitsToProduce = quantity.n.toNonNegative - stock(p).quantity
-  val gramsToProduce = unitsToProduce * weight.n.toNonNegative
+  val unitsToProduce = (quantity.n: NonNegativeNumber) - stock(p).quantity
+  val gramsToProduce = unitsToProduce * weight.n
   val quintalsToProduce = gramsToProduce.toDecimal / 100_000
-  val neededQuintals = recipeBook(cheeseType).n.toNonNegative * quintalsToProduce
+  val neededQuintals = quintalsToProduce * recipeBook(cheeseType).n
   neededQuintals.ceil.quintalsOfMilk
 
 private def magicAiEstimator(
