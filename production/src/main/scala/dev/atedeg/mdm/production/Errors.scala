3a0a08efb1bce7d326ded16ad031263d8ddf16ec
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-08-01 16:33:42 +0200

    refactor: use common definitions in utils

--- production/src/main/scala/dev/atedeg/mdm/production/Errors.scala
+++ production/src/main/scala/dev/atedeg/mdm/production/Errors.scala
@@ -1,5 +1,7 @@
 package dev.atedeg.mdm.production
 
+import dev.atedeg.mdm.products.CheeseType
+
 /**
  * Error raised in case there is no [[Recipe recipe]] for a given [[CheeseType cheese type]].
  */
--- production/src/main/scala/dev/atedeg/mdm/production/Events.scala
+++ production/src/main/scala/dev/atedeg/mdm/production/Events.scala
@@ -1,5 +1,7 @@
 package dev.atedeg.mdm.production
 
+import cats.data.NonEmptyList
+
 /**
  * The events that may be produced by the bounded context.
  */
@@ -9,10 +11,10 @@ enum OutgoingEvent:
    * [[QuintalsOfIngredient needed ingredients and the quantity]] necessary to sustain the
    * production.
    */
-  case StartProduction(neededIngredient: List[QuintalsOfIngredient])
+  case StartProduction(neededIngredient: NonEmptyList[QuintalsOfIngredient])
 
   /**
    * Fired when a [[Production.InProgress production]] is terminated, given a
    * [[LotNumber lot number]] and sent to the refrigeration room.
    */
-  case ProductionEnded(productionID: ProductionID, lotNumber: LotNumber)
+  case ProductionEnded(productionID: ProductionID, batchID: BatchID)
