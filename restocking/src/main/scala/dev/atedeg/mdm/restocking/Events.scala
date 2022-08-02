30a3ad002a31805c3d953570524bcb9c911da7d1
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-02 18:08:18 +0200

    docs: add scaladoc to events

--- restocking/src/main/scala/dev/atedeg/mdm/restocking/Events.scala
+++ restocking/src/main/scala/dev/atedeg/mdm/restocking/Events.scala
@@ -2,7 +2,17 @@ package dev.atedeg.mdm.restocking
 
 import cats.data.NonEmptyList
 
+/**
+ * The events handled by this bounded context.
+ */
 enum IncomingEvent:
+  /**
+   * Received when an order for milk has to be placed.
+   */
   case OrderMilk(quintals: QuintalsOfMilk)
-  case ProductionStarted(ingredients: NonEmptyList[QuintalsOfIngredient])
 
+  /**
+   * Received when a production is started.
+   * It consumes the given [[QuintalsOfIngredient ingredients]].
+   */
+  case ProductionStarted(ingredients: NonEmptyList[QuintalsOfIngredient])
