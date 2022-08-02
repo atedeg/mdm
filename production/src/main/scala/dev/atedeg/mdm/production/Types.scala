d0223d052660fd7eae8112cf60e36295c5e80eba
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-02 18:01:32 +0200

    refactor: move ingredients to shared kernel

--- production/src/main/scala/dev/atedeg/mdm/production/Types.scala
+++ production/src/main/scala/dev/atedeg/mdm/production/Types.scala
@@ -74,12 +74,3 @@ final case class QuintalsOfIngredient(quintals: WeightInQuintals, ingredient: In
  */
 final case class WeightInQuintals(n: PositiveDecimal) derives Times
 
-/**
- * An ingredient that may be needed by a [[Recipe recipe]] to produce a [[CheeseType type of cheese]].
- */
-enum Ingredient:
-  case Milk extends Ingredient
-  case Cream extends Ingredient
-  case Rennet extends Ingredient
-  case Salt extends Ingredient
-  case Probiotics extends Ingredient
