4b8d7b3cf0b33d9efc1c48ac66a31ad6752c492f
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-02 12:21:41 +0200

    chore: add more events to the bc

--- production-planning/src/main/scala/dev/atedeg/mdm/productionplanning/Events.scala
+++ production-planning/src/main/scala/dev/atedeg/mdm/productionplanning/Events.scala
@@ -1,7 +1,25 @@
 package dev.atedeg.mdm.productionplanning
 
+/**
+ * The events that have to be handled by the bounded context.
+ */
 enum IncomingEvent:
+  /**
+   * Event representing an order placed used to create the [[ProductionPlan production plan]] of the day.
+   */
   case NewOrderReceived(order: Order)
 
+/**
+ * The events that may be produced by the bounded context.
+ */
 enum OutgoingEvent:
+  /**
+   * Events that contains the [[ProductionPlan production plan]] of the day.
+   */
   case ProductionPlanReady(productionPlan: ProductionPlan)
+
+  /**
+   * If an order cannot be fulfilled since there are some products' ripening days takes more
+   * time than the order required date.
+   */
+  case OrderDelayed()
