dbc7db98f44382c4b3db3c8a62b55e865cc734fb
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-03 18:55:10 +0200

    refactor: removed useless extends to ingredients case class

--- products-shared-kernel/src/main/scala/dev/atedeg/mdm/products/Ingredients.scala
+++ products-shared-kernel/src/main/scala/dev/atedeg/mdm/products/Ingredients.scala
@@ -4,8 +4,8 @@ package dev.atedeg.mdm.products
  * An ingredient that may be needed by a [[Recipe recipe]] to produce a [[CheeseType type of cheese]].
  */
 enum Ingredient:
-  case Milk extends Ingredient
-  case Cream extends Ingredient
-  case Rennet extends Ingredient
-  case Salt extends Ingredient
-  case Probiotics extends Ingredient
+  case Milk
+  case Cream
+  case Rennet
+  case Salt
+  case Probiotics
