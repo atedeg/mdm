5cf7fb99af8daf8550818fa855b85b306cd597c2
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-08-03 17:57:46 +0200

    chore: add some utility extension methods
    Co-Authored-By: ndido98 <ndido98@gmail.com>

--- client-orders/src/main/scala/dev/atedeg/mdm/clientorders/utils/TypesOps.scala
+++ client-orders/src/main/scala/dev/atedeg/mdm/clientorders/utils/TypesOps.scala
@@ -3,18 +3,24 @@ package dev.atedeg.mdm.clientorders.utils
 import cats.kernel.Order
 import eu.timepit.refined.auto.autoUnwrap
 
-import dev.atedeg.mdm.clientorders.PalletizedQuantity
-import dev.atedeg.mdm.clientorders.PalletizedQuantity.apply
-import dev.atedeg.mdm.clientorders.PriceInEuroCents
-import dev.atedeg.mdm.clientorders.Quantity
+import dev.atedeg.mdm.clientorders.*
 import dev.atedeg.mdm.utils.*
 import dev.atedeg.mdm.utils.given
 
-extension (n: PositiveNumber) def euroCents: PriceInEuroCents = PriceInEuroCents(n)
-extension (i: NonNegativeNumber) def palletizedQuantity: PalletizedQuantity = PalletizedQuantity(i)
+extension (n: PositiveNumber)
+  def euroCents: PriceInEuroCents = PriceInEuroCents(n)
+  def quantity: Quantity = Quantity(n)
+
+extension (i: NonNegativeNumber)
+  def palletizedQuantity: PalletizedQuantity = PalletizedQuantity(i)
+  def missingQuantity: MissingQuantity = MissingQuantity(i)
 
 object QuantityOps:
-  extension (q: Quantity) def toPalletizedQuantity: PalletizedQuantity = PalletizedQuantity(q.n)
+
+  extension (q: Quantity)
+    def toPalletizedQuantity: PalletizedQuantity = PalletizedQuantity(q.n)
+    def toMissingQuantity: MissingQuantity = MissingQuantity(q.n)
 
   given Order[Quantity] with
     override def compare(x: Quantity, y: Quantity): Int = Order[Int].compare(x.n, y.n)
+
