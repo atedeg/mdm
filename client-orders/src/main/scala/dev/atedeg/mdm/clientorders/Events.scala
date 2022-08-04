bc033b98073c0330ac5802f9dde58501b2fa97d0
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-04 12:30:05 +0200

    feat: add new outgoing event

--- client-orders/src/main/scala/dev/atedeg/mdm/clientorders/Events.scala
+++ client-orders/src/main/scala/dev/atedeg/mdm/clientorders/Events.scala
@@ -1,15 +1,26 @@
 package dev.atedeg.mdm.clientorders
 
+import java.time.LocalDateTime
+
+import cats.data.NonEmptyList
+
 import dev.atedeg.mdm.products.Product
 
 /**
  * The events that have to be handled by the bounded context.
  */
 enum IncomingEvent:
+
   /**
-   * An [[IncomingEvent event]] which is received when an [[order.IncomingOrder order]] is made.
+   * An [[IncomingEvent event]] which is received when an [[Order.IncomingOrder order]] is made.
    */
-  case OrderReceived()
+  case OrderReceived(
+      id: OrderID,
+      orderLines: NonEmptyList[IncomingOrderLine],
+      customer: Customer,
+      deliveryDate: LocalDateTime,
+      deliveryLocation: Location,
+  )
 
   /**
    * An [[IncomingEvent event]] received when an operator takes a [[Product product]] from the stock and palletizes it
@@ -18,7 +29,16 @@ enum IncomingEvent:
   case ProductPalletizedForOrder(orderID: OrderID, quantity: Quantity, product: Product)
 
   /**
-   * An [[IncomingEvent event]] received when an [[order.InProgressOrder order in progress]] is marked as
+   * An [[IncomingEvent event]] received when an [[Order.InProgressOrder order in progress]] is marked as
    * [[order.CompletedOrder ready to be shipped]].
    */
   case OrderCompleted(orderID: OrderID)
+
+/**
+ * The events that may be produced by the bounded context.
+ */
+enum OutgoingEvent:
+  /**
+   * An event emitted when a new [[IncomingOrder incoming order]] is received and processed.
+   */
+  case OrderReceived(incomingOrder: IncomingOrder)
