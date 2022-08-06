83848acefc49abe12e45db5298f2f7c08f63ebb3
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-08-06 23:26:55 +0200

    refactor: change DTO structure

--- restocking/src/main/scala/dev/atedeg/mdm/restocking/dto/DTOs.scala
+++ restocking/src/main/scala/dev/atedeg/mdm/restocking/dto/DTOs.scala
@@ -4,18 +4,20 @@ import dev.atedeg.mdm.products.utils.IngredientDTO.given
 import dev.atedeg.mdm.restocking.*
 import dev.atedeg.mdm.restocking.IncomingEvent.*
 import dev.atedeg.mdm.utils.serialization.DTO
+import dev.atedeg.mdm.utils.serialization.DTOGenerators.*
+import dev.atedeg.mdm.utils.serialization.DTOOps.*
 
 final case class OrderMilkDTO(quintalsOfMilk: Int)
 final case class ProductionStartedDTO(quintalsOfIngredients: List[QuintalsOfIngredientDTO])
 final case class QuintalsOfIngredientDTO(quintals: Double, ingredient: String)
 
 object OrderMilkDTO:
-  given DTO[OrderMilk, OrderMilkDTO] = DTO.interCaseClassDTO
-  private given DTO[QuintalsOfMilk, Int] = DTO.caseClassDTO
+  given DTO[OrderMilk, OrderMilkDTO] = interCaseClassDTO
+  private given DTO[QuintalsOfMilk, Int] = caseClassDTO
 
 object ProductionStartedDTO:
-  given DTO[ProductionStarted, ProductionStartedDTO] = DTO.interCaseClassDTO
+  given DTO[ProductionStarted, ProductionStartedDTO] = interCaseClassDTO
 
 object QuintalsOfIngredientDTO:
-  given DTO[QuintalsOfIngredient, QuintalsOfIngredientDTO] = DTO.interCaseClassDTO
-  private given DTO[WeightInQuintals, Double] = DTO.caseClassDTO
+  given DTO[QuintalsOfIngredient, QuintalsOfIngredientDTO] = interCaseClassDTO
+  private given DTO[WeightInQuintals, Double] = caseClassDTO
--- utils/src/main/scala/dev/atedeg/mdm/utils/serialization/DTO.scala
+++ utils/src/main/scala/dev/atedeg/mdm/utils/serialization/DTO.scala
@@ -17,53 +17,56 @@ trait DTO[E, D]:
   def elemToDto(e: E): D
   def dtoToElem(dto: D): Either[String, E]
 
-object DTO:
-  extension [E](e: E) def toDTO[D](using d: DTO[E, D]) = d.elemToDto(e)
+object DTOOps:
   extension [D](dto: D) def toDomain[E](using d: DTO[E, D]): Either[String, E] = d.dtoToElem(dto)
+  extension [E](e: E) def toDTO[D](using d: DTO[E, D]) = d.elemToDto(e)
+
+object DTO:
+  import DTOGenerators.*
+  import DTOOps.*
+
+  given DTO[Int, Int] = idDTO
+  given DTO[Double, Double] = idDTO
+  given DTO[String, String] = idDTO
 
   given listDTO[E, D](using DTO[E, D]): DTO[List[E], List[D]] with
     override def dtoToElem(dto: List[D]): Either[String, List[E]] = dto.traverse(_.toDomain)
     override def elemToDto(e: List[E]): List[D] = e.map(_.toDTO)
 
   given nonEmptyListDTO[E, D](using DTO[E, D]): DTO[NonEmptyList[E], List[D]] with
+    override def elemToDto(e: NonEmptyList[E]): List[D] = e.toList.map(_.toDTO)
     override def dtoToElem(dto: List[D]): Either[String, NonEmptyList[E]] =
       dto.toNel.toRight("Got an empty list, it should contain at least one element").flatMap(_.traverse(_.toDomain))
 
-    override def elemToDto(e: NonEmptyList[E]): List[D] = e.toList.map(_.toDTO)
-
   given DTO[UUID, String] with
-    override def dtoToElem(dto: String): Either[String, UUID] =
-      Try(UUID.fromString(dto)).toEither.leftMap(_ => s"Invalid UUID: $dto")
     @SuppressWarnings(Array("org.wartremover.warts.ToString"))
     override def elemToDto(e: UUID): String = e.toString
+    override def dtoToElem(dto: String): Either[String, UUID] =
+      Try(UUID.fromString(dto)).toEither.leftMap(_ => s"Invalid UUID: $dto")
 
   given DTO[LocalDate, String] with
+    override def elemToDto(e: LocalDate): String = e.format(DateTimeFormatter.ISO_LOCAL_DATE)
     override def dtoToElem(dto: String): Either[String, LocalDate] =
       Try(LocalDate.parse(dto, DateTimeFormatter.ISO_LOCAL_DATE)).toEither.leftMap(_ => s"Invalid date: $dto")
-    override def elemToDto(e: LocalDate): String = e.format(DateTimeFormatter.ISO_LOCAL_DATE)
 
   given DTO[LocalDateTime, String] with
+    override def elemToDto(e: LocalDateTime): String = e.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
     override def dtoToElem(dto: String): Either[String, LocalDateTime] =
       Try(LocalDateTime.parse(dto, DateTimeFormatter.ISO_LOCAL_DATE_TIME)).toEither.leftMap(_ => s"Invalid date: $dto")
-    override def elemToDto(e: LocalDateTime): String = e.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
 
   given refinedDTO[E, D, P: ValidFor[E]](using d: DTO[E, D]): DTO[E Refined P, D] with
-    override def dtoToElem(dto: D): Either[String, E Refined P] = d.dtoToElem(dto).flatMap(_.refined[P])
     override def elemToDto(e: E Refined P): D = e.value.toDTO
+    override def dtoToElem(dto: D): Either[String, E Refined P] = d.dtoToElem(dto).flatMap(_.refined[P])
 
-  given DTO[Int, Int] = idDTO
-  given DTO[Double, Double] = idDTO
-  given DTO[String, String] = idDTO
-
+object DTOGenerators:
   def idDTO[T]: DTO[T, T] = new DTO[T, T]:
-    override def dtoToElem(dto: T): Either[String, T] = dto.asRight[String]
     override def elemToDto(e: T): T = e
+    override def dtoToElem(dto: T): Either[String, T] = dto.asRight[String]
 
-  inline private def numberOfFields[A](inline p: Mirror.ProductOf[A]): Int = constValue[Tuple.Size[p.MirroredElemTypes]]
   @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
   private def firstField[T, X](t: T): X = t.asInstanceOf[Product].productElement(0).asInstanceOf[X]
-
   private type First[T <: Tuple] = Tuple.Elem[T, 0]
+  inline private def numberOfFields[A](inline p: Mirror.ProductOf[A]): Int = constValue[Tuple.Size[p.MirroredElemTypes]]
 
   inline def caseClassDTO[E, D](using p: Mirror.ProductOf[E]): DTO[E, D] =
     inline numberOfFields(p) match
@@ -72,6 +75,7 @@ object DTO:
         val instance: DTO[t, D] = summonInline[DTO[t, D]]
         new DTO[E, D]:
           def elemToDto(e: E): D = instance.elemToDto(firstField(e))
+
           def dtoToElem(dto: D): Either[String, E] = instance.dtoToElem(dto).map(e => p.fromProduct(e *: EmptyTuple))
       case _ => compiletime.error("Can only derive for case classes with only one field")
 
@@ -87,6 +91,7 @@ object DTO:
         override def elemToDto(e: C1): C2 =
           val fields = e.asInstanceOf[Product].productIterator.zip(instances).map(fi => fi._2.elemToDto(fi._1)).toArray
           p2.fromProduct(Tuple.fromArray(fields))
+
         override def dtoToElem(dto: C2): Either[String, C1] = dto
           .asInstanceOf[Product]
           .productIterator
