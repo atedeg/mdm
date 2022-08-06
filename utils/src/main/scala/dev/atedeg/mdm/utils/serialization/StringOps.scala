be3bc33bad8139bcc4c9b752bec8b6b892facead
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-08-06 23:11:38 +0200

    style: reformat build.sbt

--- utils/src/main/scala/dev/atedeg/mdm/utils/serialization/StringOps.scala
+++
@@ -1,9 +0,0 @@
-package dev.atedeg.mdm.utils.serialization
-
-trait Show[A]:
-  def toShow(a: A): String
-  extension (a: A) def show: String = toShow(a)
-
-trait Read[A]:
-  def fromString(s: String): Either[String, A]
-  extension (s: String) def read: Either[String, A] = fromString(s)
