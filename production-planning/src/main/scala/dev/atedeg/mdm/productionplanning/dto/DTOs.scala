d3ae700dc71a14edb7a2853d998f6e7ecea944c4
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-07 15:35:48 +0200

    feat: add production planning DTOs

--- production-planning/src/main/scala/dev/atedeg/mdm/productionplanning/dto/DTOs.scala
+++ production-planning/src/main/scala/dev/atedeg/mdm/productionplanning/dto/DTOs.scala
@@ -1,14 +1,44 @@
 package dev.atedeg.mdm.productionplanning.dto
 
-type ProductDTO
+import java.time.LocalDate
+
+import dev.atedeg.mdm.productionplanning.*
+import dev.atedeg.mdm.productionplanning.IncomingEvent.*
+import dev.atedeg.mdm.productionplanning.OutgoingEvent.*
+import dev.atedeg.mdm.productionplanning.dto.OrderIDDTO.given
+import dev.atedeg.mdm.productionplanning.dto.QuantityDTO.given
+import dev.atedeg.mdm.products.dto.ProductDTO
+import dev.atedeg.mdm.products.dto.ProductDTO.given
+import dev.atedeg.mdm.utils.serialization.DTO
+import dev.atedeg.mdm.utils.serialization.DTOGenerators.*
+import dev.atedeg.mdm.utils.serialization.DTOOps.*
+
 // incoming events
 final case class NewOrderReceivedDTO(order: OrderDTO)
 final case class OrderDTO(orderID: String, requiredBy: String, orderedProducts: List[OrderedProductDTO])
 final case class OrderedProductDTO(product: ProductDTO, quantity: Int)
+
+object NewOrderReceivedDTO:
+  given DTO[NewOrderReceived, NewOrderReceivedDTO] = interCaseClassDTO
+  private given DTO[Order, OrderDTO] = interCaseClassDTO
+  private given DTO[OrderedProduct, OrderedProductDTO] = interCaseClassDTO
 
 // outgoing events
 final case class ProductionPlanReadyDTO(productionPlan: ProductionPlanDTO)
 final case class ProductionPlanDTO(productsToProduce: List[ProductToProduceDTO])
 final case class ProductToProduceDTO(product: ProductDTO, quantity: Int)
 
-final case class OrderDelayed(orderID: String, newDeliveryDate: String)
+object ProductionPlanReadyDTO:
+  given DTO[ProductionPlanReady, ProductionPlanReadyDTO] = interCaseClassDTO
+  given DTO[ProductionPlan, ProductionPlanDTO] = interCaseClassDTO
+  given DTO[ProductToProduce, ProductToProduceDTO] = interCaseClassDTO
+
+final case class OrderDelayedDTO(orderID: String, newDeliveryDate: String)
+object OrderDelayedDTO:
+  given DTO[OrderDelayed, OrderDelayedDTO] = interCaseClassDTO
+
+private object QuantityDTO:
+  given DTO[Quantity, Int] = caseClassDTO
+
+private object OrderIDDTO:
+  given DTO[OrderID, String] = caseClassDTO
