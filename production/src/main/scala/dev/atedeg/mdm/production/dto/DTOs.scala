e4967365dc70cba518110016fe81ee3b55378ef6
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-08-07 19:00:18 +0200

    refactor: autogenerate all DTOs implementations

--- production/src/main/scala/dev/atedeg/mdm/production/dto/DTOs.scala
+++ production/src/main/scala/dev/atedeg/mdm/production/dto/DTOs.scala
@@ -1,6 +1,14 @@
 package dev.atedeg.mdm.production.dto
 
-type ProductDTO = String
+import dev.atedeg.mdm.production.*
+import dev.atedeg.mdm.production.IncomingEvent.*
+import dev.atedeg.mdm.production.OutgoingEvent.*
+import dev.atedeg.mdm.products.dto.IngredientDTO.given
+import dev.atedeg.mdm.products.dto.ProductDTO
+import dev.atedeg.mdm.products.dto.ProductDTO.given
+import dev.atedeg.mdm.utils.serialization.DTO
+import dev.atedeg.mdm.utils.serialization.DTOGenerators.*
+import dev.atedeg.mdm.utils.serialization.DTOOps
 
 final case class StartProductionDTO(neededIngredients: List[QuintalsOfIngredientDTO])
 final case class QuintalsOfIngredientDTO(quintals: Double, ingredient: String)
@@ -8,3 +16,18 @@ final case class ProductionEndedDTO(productionID: String, batchID: String)
 final case class ProductionPlanReadyDTO(productionPlan: List[ProductionPlanItemDTO])
 final case class ProductionPlanItemDTO(product: ProductDTO, units: Int)
 
+object StartProductionDTO:
+  given DTO[StartProduction, StartProductionDTO] = interCaseClassDTO
+  private given DTO[QuintalsOfIngredient, QuintalsOfIngredientDTO] = interCaseClassDTO
+  private given DTO[WeightInQuintals, Double] = caseClassDTO
+
+object ProductionEndedDTO:
+  given DTO[ProductionEnded, ProductionEndedDTO] = interCaseClassDTO
+  private given DTO[ProductionID, String] = caseClassDTO
+  private given DTO[BatchID, String] = caseClassDTO
+
+object ProductionPlanReadyDTO:
+  given DTO[ProductionPlanReady, ProductionPlanReadyDTO] = interCaseClassDTO
+  private given DTO[ProductionPlan, List[ProductionPlanItemDTO]] = caseClassDTO
+  private given DTO[ProductionPlanItem, ProductionPlanItemDTO] = interCaseClassDTO
+  private given DTO[NumberOfUnits, Int] = caseClassDTO
