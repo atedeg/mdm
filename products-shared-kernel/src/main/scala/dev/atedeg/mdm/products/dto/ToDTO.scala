7a8daaff68dc62becc3c91fae818b7079e5a6567
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-07 15:14:42 +0200

    refactor: automatically generate products and cheese type DTOs

--- products-shared-kernel/src/main/scala/dev/atedeg/mdm/products/dto/ToDTO.scala
+++
@@ -1,6 +0,0 @@
-package dev.atedeg.mdm.products.dto
-
-import dev.atedeg.mdm.products.Product
-import dev.atedeg.mdm.products.utils.ReadShowInstances.given
-
-extension (p: Product) def toDTO: ProductDTO = ProductDTO(p.cheeseType.show, p.weight.n.value)
