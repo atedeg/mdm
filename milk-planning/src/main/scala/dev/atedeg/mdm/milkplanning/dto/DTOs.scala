1bccf3800eb495c30739fac7b893dffee7a8ec37
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-08-08 09:19:49 +0200

    refactor: use new utilities to generate milk-planning DTOs

--- milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/dto/DTOs.scala
+++ milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/dto/DTOs.scala
@@ -1,7 +1,25 @@
 package dev.atedeg.mdm.milkplanning.dto
 
+import dev.atedeg.mdm.milkplanning.{ Quantity, QuintalsOfMilk, RequestedProduct }
+import dev.atedeg.mdm.milkplanning.IncomingEvent.ReceivedOrder
+import dev.atedeg.mdm.milkplanning.OutgoingEvent.OrderMilk
 import dev.atedeg.mdm.products.dto.ProductDTO
+import dev.atedeg.mdm.utils.serialization.DTO
+import dev.atedeg.mdm.utils.serialization.DTOGenerators.*
+import dev.atedeg.mdm.utils.serialization.DTOOps.*
 
 final case class ReceivedOrderDTO(products: List[RequestedProductDTO])
 final case class RequestedProductDTO(product: ProductDTO, quantity: Int, requiredBy: String)
 final case class OrderMilkDTO(quintals: Int)
+
+object ReceivedOrderDTO:
+  import dev.atedeg.mdm.milkplanning.dto.RequestedProductDTO.given
+  given DTO[ReceivedOrder, ReceivedOrderDTO] = interCaseClassDTO
+
+object RequestedProductDTO:
+  given DTO[RequestedProduct, RequestedProductDTO] = interCaseClassDTO
+  private given DTO[Quantity, Int] = caseClassDTO
+
+object OrderMilkDTO:
+  given DTO[OrderMilk, OrderMilkDTO] = interCaseClassDTO
+  private given DTO[QuintalsOfMilk, Int] = caseClassDTO
