e5668693eb6d788d86a31cb655786f94e7517b3e
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-08-08 17:14:27 +0200

    chore: use stock repository

--- restocking/src/main/scala/dev/atedeg/mdm/restocking/api/Handlers.scala
+++ restocking/src/main/scala/dev/atedeg/mdm/restocking/api/Handlers.scala
@@ -9,15 +9,16 @@ import dev.atedeg.mdm.restocking.*
 import dev.atedeg.mdm.restocking.IncomingEvent.*
 import dev.atedeg.mdm.restocking.Stock
 import dev.atedeg.mdm.restocking.api.dto.RemainingMilkDTO
+import dev.atedeg.mdm.restocking.api.repositories.StockRepository
 import dev.atedeg.mdm.restocking.dto.{ OrderMilkDTO, ProductionStartedDTO, StockDTO }
 import dev.atedeg.mdm.restocking.dto.StockDTO.given
 import dev.atedeg.mdm.utils.monads.*
 import dev.atedeg.mdm.utils.serialization.DTOOps.*
 
-final case class DBClient()
-def remaningQuintalsOfMilkHandler[M[_]: Monad: LiftIO: CanRead[DBClient]: CanRaise[String]]: M[RemainingMilkDTO] =
+def remaningQuintalsOfMilkHandler[M[_]: Monad: LiftIO: CanRead[StockRepository]: CanRaise[String]]
+    : M[RemainingMilkDTO] =
   for
-    remainingMilkDTO <- readQuintalsFromDB
+    remainingMilkDTO <- readState >>= (_.getQuintals)
     remainingMilk <- remainingMilkDTO.toDomain[RemainingMilk].getOrRaise
   yield remainingMilk.toDTO[RemainingMilkDTO]
 
@@ -27,17 +28,15 @@ def orderMilkHandler[M[_]: Monad: LiftIO: CanRaise[String]](orderMilkDTO: OrderM
     _ <- makeMilkOrder(orderMilk.toDTO[OrderMilkDTO])
   yield ()
 
-def productionStartedHandler[M[_]: Monad: LiftIO: CanRaise[String]](
+def productionStartedHandler[M[_]: Monad: LiftIO: CanRaise[String]: CanRead[StockRepository]](
     productionStartedDTO: ProductionStartedDTO,
 ): M[Unit] =
   for
+    stockRepository <- readState
     productionStarted <- productionStartedDTO.toDomain[ProductionStarted].getOrRaise
-    stock <- readStockFromDB >>= (_.toDomain[Stock].getOrRaise)
+    stock <- stockRepository.getStock >>= (_.toDomain[Stock].getOrRaise)
     newStock = consumeIngredients(stock)(productionStarted.ingredients)
-    _ <- writeStockToDB(newStock.toDTO[StockDTO])
+    _ <- stockRepository.writeStock(newStock.toDTO[StockDTO])
   yield ()
 
-private def readQuintalsFromDB[M[_]: Monad: LiftIO]: M[RemainingMilkDTO] = RemainingMilkDTO(10).pure
 private def makeMilkOrder[M[_]: Monad: LiftIO](orderMilkDTO: OrderMilkDTO): M[Unit] = ().pure
-private def readStockFromDB[M[_]: Monad: LiftIO]: M[StockDTO] = Map("Milk" -> 10.2).pure
-private def writeStockToDB[M[_]: Monad: LiftIO](newStock: StockDTO): M[Unit] = ().pure
--- restocking/src/main/scala/dev/atedeg/mdm/restocking/api/endpoints/Endpoints.scala
+++ restocking/src/main/scala/dev/atedeg/mdm/restocking/api/endpoints/Endpoints.scala
@@ -9,12 +9,13 @@ import sttp.tapir.generic.auto.*
 import sttp.tapir.json.circe.jsonBody
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 
-import dev.atedeg.mdm.restocking.api.{ remaningQuintalsOfMilkHandler, DBClient }
 import dev.atedeg.mdm.restocking.api.dto.RemainingMilkDTO
+import dev.atedeg.mdm.restocking.api.remaningQuintalsOfMilkHandler
+import dev.atedeg.mdm.restocking.api.repositories.{ DBStockRepository, StockRepository }
 import dev.atedeg.mdm.utils.monads.ServerAction
 
 object RemainingQuintalsOfMilkEndpoint:
-  private val handler: ServerAction[DBClient, String, RemainingMilkDTO] = remaningQuintalsOfMilkHandler
+  private val handler: ServerAction[StockRepository, String, RemainingMilkDTO] = remaningQuintalsOfMilkHandler
 
   @SuppressWarnings(Array("org.wartremover.warts.Any"))
   val remainingQuintalsOfMilkEndpoint: PublicEndpoint[Unit, String, RemainingMilkDTO, Any] =
@@ -24,5 +25,5 @@ object RemainingQuintalsOfMilkEndpoint:
       .errorOut(stringBody)
 
   val remainingQuintalsOfMilkRoute: HttpRoutes[IO] = Http4sServerInterpreter[IO]().toRoutes(
-    remainingQuintalsOfMilkEndpoint.serverLogic(_ => handler.value.run(DBClient())),
+    remainingQuintalsOfMilkEndpoint.serverLogic(_ => handler.value.run(DBStockRepository("conn-string"))),
   )
