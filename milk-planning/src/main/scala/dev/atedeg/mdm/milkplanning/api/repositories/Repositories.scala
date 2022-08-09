040067d8c1bb097b8f9e89b0e50a23267cd44813
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-08-09 17:21:52 +0200

    style: reformat files

--- milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/api/repositories/Repositories.scala
+++ milk-planning/src/main/scala/dev/atedeg/mdm/milkplanning/api/repositories/Repositories.scala
@@ -2,8 +2,9 @@ package dev.atedeg.mdm.milkplanning.api.repositories
 
 import cats.Monad
 import cats.effect.LiftIO
+
 import dev.atedeg.mdm.milkplanning.QuintalsOfMilk
-import dev.atedeg.mdm.milkplanning.dto.{ReceivedOrderDTO, RecipeBookDTO, RequestedProductDTO}
+import dev.atedeg.mdm.milkplanning.dto.{ ReceivedOrderDTO, RecipeBookDTO, RequestedProductDTO }
 
 trait RecipeBookRepository:
   def getRecipeBook[M[_]: Monad: LiftIO]: M[RecipeBookDTO]
@@ -11,4 +12,3 @@ trait RecipeBookRepository:
 trait ReceivedOrderRepository:
   def save[M[_]: Monad: LiftIO](requestedProducts: List[RequestedProductDTO]): M[Unit]
   def getRequestedProducts[M[_]: Monad: LiftIO]: M[List[RequestedProductDTO]]
-
