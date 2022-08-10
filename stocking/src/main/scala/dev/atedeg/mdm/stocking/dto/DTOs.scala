0a9ddffb61ebeae9b9abeb07cc1fd9e57d63e4b4
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-10 18:42:19 +0200

    chore: more stocking dtos

--- stocking/src/main/scala/dev/atedeg/mdm/stocking/dto/DTOs.scala
+++ stocking/src/main/scala/dev/atedeg/mdm/stocking/dto/DTOs.scala
@@ -18,15 +18,41 @@ object ProductStockedDTO:
 
 private given DTO[BatchID, String] = caseClassDTO
 
-final case class BatchReadyForQualityAssuranceDTO(batch: String)
-object BatchReadyForQualityAssuranceDTO:
-  given DTO[BatchReadyForQualityAssurance, BatchReadyForQualityAssuranceDTO] = interCaseClassDTO
+final case class BatchReadyForQualityAssuranceEventDTO(batch: String)
+object BatchReadyForQualityAssuranceEventDTO:
+  given DTO[BatchReadyForQualityAssurance, BatchReadyForQualityAssuranceEventDTO] = interCaseClassDTO
 
 final case class ProductRemovedFromStockDTO(quantity: Int, product: ProductDTO)
 object ProductRemovedFromStockDTO:
   given DTO[ProductRemovedFromStock, ProductRemovedFromStockDTO] = interCaseClassDTO
-  given DTO[DesiredQuantity, Int] = caseClassDTO
+  given DTO[Quantity, Int] = caseClassDTO
 
 final case class NewBatchDTO(batchID: String, cheeseType: String, readyFrom: String)
 object NewBatchDTO:
   given DTO[NewBatch, NewBatchDTO] = interCaseClassDTO
+
+final case class AvailableStockDTO(as: List[(ProductDTO, Int)])
+object AvailableStockDTO:
+  given DTO[AvailableStock, AvailableStockDTO] = interCaseClassDTO
+  private given DTO[AvailableQuantity, Int] = caseClassDTO
+
+final case class DesiredStockDTO(ds: List[(ProductDTO, Int)])
+object DesiredStockDTO:
+  given DTO[DesiredStock, DesiredStockDTO] = interCaseClassDTO
+  private given DTO[DesiredQuantity, Int] = caseClassDTO
+
+final case class AgingBatchDTO(batchID: String, cheeseType: String, readyFrom: String)
+object AgingBatchDTO:
+  given DTO[Batch.Aging, AgingBatchDTO] = interCaseClassDTO
+
+final case class QualityAssuredBatchPassedDTO(id: String, cheeseType: String)
+object QualityAssuredBatchPassedDTO:
+  given DTO[QualityAssuredBatch.Passed, QualityAssuredBatchPassedDTO] = interCaseClassDTO
+
+final case class QualityAssuredBatchFailedDTO(id: String, cheeseType: String)
+object QualityAssuredBatchFailedDTO:
+  given DTO[QualityAssuredBatch.Failed, QualityAssuredBatchFailedDTO] = interCaseClassDTO  
+
+final case class BatchReadyForQualityAssuranceDTO(id: String, cheeseType: String)
+object BatchReadyForQualityAssuranceDTO:
+  given DTO[Batch.ReadyForQualityAssurance, BatchReadyForQualityAssuranceDTO] = interCaseClassDTO
