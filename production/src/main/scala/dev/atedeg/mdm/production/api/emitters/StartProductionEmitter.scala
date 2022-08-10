dd84953890c7966fbf0a4301fc03602734715e03
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-08-10 15:54:52 +0200

    refactor: add missing handler

--- production/src/main/scala/dev/atedeg/mdm/production/api/emitters/StartProductionEmitter.scala
+++ production/src/main/scala/dev/atedeg/mdm/production/api/emitters/StartProductionEmitter.scala
@@ -3,7 +3,8 @@ package dev.atedeg.mdm.production.api.emitters
 import cats.Monad
 import cats.effect.LiftIO
 
-import dev.atedeg.mdm.production.dto.StartProductionDTO
+import dev.atedeg.mdm.production.dto.*
 
-trait StartProductionEmitter:
-  def emit[M[_]: Monad: LiftIO](message: StartProductionDTO): M[Unit]
+trait Emitter:
+  def emitStart[M[_]: Monad: LiftIO](message: StartProductionDTO): M[Unit]
+  def emitEnded[M[_]: Monad: LiftIO](message: ProductionEndedDTO): M[Unit]
