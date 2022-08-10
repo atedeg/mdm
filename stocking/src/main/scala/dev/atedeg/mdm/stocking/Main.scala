42712a2dc9842485a21ed84ac52d3577a964d322
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-10 19:10:01 +0200

    chore: add logger

--- stocking/src/main/scala/dev/atedeg/mdm/stocking/Main.scala
+++ stocking/src/main/scala/dev/atedeg/mdm/stocking/Main.scala
@@ -7,6 +7,9 @@ import cats.syntax.all.*
 import org.http4s.HttpRoutes
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.server.Router
+import org.typelevel.log4cats.*
+import org.typelevel.log4cats.slf4j.Slf4jFactory
+import org.typelevel.log4cats.slf4j.loggerFactoryforSync
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
 
@@ -23,10 +26,13 @@ object Main extends IOApp:
   private val routes: HttpRoutes[IO] =
     desiredStockRoute <+> availableStockRoute <+> approveBatchRoute <+> rejectBatchRoute <+> swaggerRoute
 
+  implicit val logging: LoggerFactory[IO] = Slf4jFactory[IO]
+  val logger: SelfAwareStructuredLogger[IO] = LoggerFactory[IO].getLogger
+
   override def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO]
       .bindHttp(Properties.envOrElse("PORT", "8080").toInt, Properties.envOrElse("HOST", "localhost"))
       .withHttpApp(Router("/" -> routes).orNotFound)
       .resource
-      .use(_ => IO.never[Unit])
+      .use(_ => logger.info("Server started") >> IO.never[Unit])
       .as(ExitCode.Success)
