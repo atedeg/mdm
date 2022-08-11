aad6bfffabedd03b771e2cb4d7d837fbebde09cb
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-11 17:15:39 +0200

    fix: date dto in prod planning acl

--- production-planning/src/main/scala/dev/atedeg/mdm/productionplanning/api/acl/DTOs.scala
+++ production-planning/src/main/scala/dev/atedeg/mdm/productionplanning/api/acl/DTOs.scala
@@ -1,11 +1,13 @@
 package dev.atedeg.mdm.productionplanning.api.acl
 
-import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 import cats.data.NonEmptyList
 
 import dev.atedeg.mdm.productionplanning.dto.*
 import dev.atedeg.mdm.products.dto.ProductDTO
+import dev.atedeg.mdm.utils.serialization.DTOOps.toDTO
 
 final case class IncomingOrderLineDTO(quantity: Int, product: ProductDTO)
 final case class CustomerDTO(code: String, name: String, vatNumber: String)
@@ -23,7 +25,8 @@ extension (iol: IncomingOrderLineDTO)
 
 extension (io: IncomingOrderDTO)
   def toNewOrderDTO: OrderDTO =
-    OrderDTO(io.id, io.deliveryDate, io.orderLines.map(_.toOrderedProductDTO))
+    val date = LocalDateTime.parse(io.deliveryDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME).toLocalDate
+    OrderDTO(io.id, date.toDTO[String], io.orderLines.map(_.toOrderedProductDTO))
 
 extension (io: IncomingOrderDTO) def toNewOrderReceivedDTO: NewOrderReceivedDTO = NewOrderReceivedDTO(io.toNewOrderDTO)
 
