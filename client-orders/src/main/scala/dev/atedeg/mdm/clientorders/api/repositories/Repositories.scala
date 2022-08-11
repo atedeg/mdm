2c430573073b61ab8d192455f3ef6ea9e0d7c8a5
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-08-11 16:26:15 +0200

    chore: add handler for order completion

--- client-orders/src/main/scala/dev/atedeg/mdm/clientorders/api/repositories/Repositories.scala
+++ client-orders/src/main/scala/dev/atedeg/mdm/clientorders/api/repositories/Repositories.scala
@@ -13,6 +13,7 @@ trait PriceListRepository:
 trait OrderRepository:
   def writeInProgressOrder[M[_]: Monad: LiftIO](inProgressOrder: InProgressOrderDTO): M[Unit]
   def readInProgressOrder[M[_]: Monad: LiftIO: CanRaise[String]](orderID: String): M[InProgressOrderDTO]
+  def updateOrderToCompleted[M[_]: Monad: LiftIO: CanRaise[String]](completedOrder: CompletedOrderDTO): M[Unit]
 
 final case class PriceListRepositoryDB(connectionString: String) extends PriceListRepository:
   override def read[M[_]: Monad: LiftIO]: M[PriceListDTO] = ???
@@ -20,3 +21,4 @@ final case class PriceListRepositoryDB(connectionString: String) extends PriceLi
 final case class OrderRepositoryDB(connectionString: String) extends OrderRepository:
   override def writeInProgressOrder[M[_]: Monad: LiftIO](inProgressOrder: InProgressOrderDTO): M[Unit] = ???
   override def readInProgressOrder[M[_]: Monad: LiftIO: CanRaise[String]](orderID: String): M[InProgressOrderDTO] = ???
+  override def updateOrderToCompleted[M[_]: Monad: LiftIO: CanRaise[String]](order: CompletedOrderDTO): M[Unit] = ???
