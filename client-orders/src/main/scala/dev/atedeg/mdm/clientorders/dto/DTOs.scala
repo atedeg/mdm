2c454b605392e73520ef6965d340e47711da545a
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2022-08-11 12:02:27 +0200

    refactor: add DTO for in progress orders

--- client-orders/src/main/scala/dev/atedeg/mdm/clientorders/dto/DTOs.scala
+++ client-orders/src/main/scala/dev/atedeg/mdm/clientorders/dto/DTOs.scala
@@ -1,11 +1,14 @@
 package dev.atedeg.mdm.clientorders.dto
 
+import cats.syntax.all.*
+
 import dev.atedeg.mdm.clientorders.*
 import dev.atedeg.mdm.clientorders.IncomingEvent.*
 import dev.atedeg.mdm.clientorders.OutgoingEvent.*
 import dev.atedeg.mdm.products.dto.ProductDTO
 import dev.atedeg.mdm.utils.serialization.DTO
 import dev.atedeg.mdm.utils.serialization.DTOGenerators.*
+import dev.atedeg.mdm.utils.serialization.DTOOps.*
 
 private object Commons:
   final case class CustomerDTO(code: String, name: String, vatNumber: String)
@@ -23,6 +26,7 @@ private object Commons:
   given DTO[VATNumber, String] = caseClassDTO
   given DTO[IncomingOrderLine, IncomingOrderLineDTO] = interCaseClassDTO
   given DTO[PriceInEuroCents, Int] = caseClassDTO
+  given DTO[PalletizedQuantity, Int] = caseClassDTO
 
 import Commons.*
 import Commons.given
@@ -60,19 +64,41 @@ final case class PriceListDTO(priceList: Map[ProductDTO, Int])
 object PriceListDTO:
   given DTO[PriceList, PriceListDTO] = interCaseClassDTO
 
-final case class PricedOrderDTO(
+final case class InProgressOrderDTO(
     id: String,
-    orderLines: List[PricedOrderLineDTO],
+    orderLines: List[InProgressOrderLineDTO],
     customer: CustomerDTO,
     deliveryDate: String,
     deliveryLocation: LocationDTO,
     totalPrice: Int,
 )
-final case class PricedOrderLineDTO(quantity: Int, product: ProductDTO, totalPrice: Int)
+final case class InProgressOrderLineDTO(
+    tag: String,
+    completeDTO: Option[CompleteOrderLineDTO],
+    incompleteDTO: Option[IncompleteOrderLineDTO],
+)
 
-object PricedOrderDTO:
-  given DTO[PricedOrder, PricedOrderDTO] = interCaseClassDTO
-  private given DTO[PricedOrderLine, PricedOrderLineDTO] = interCaseClassDTO
+final case class CompleteOrderLineDTO(quantity: Int, product: ProductDTO, price: Int)
+final case class IncompleteOrderLineDTO(actual: Int, required: Int, product: ProductDTO, price: Int)
+
+object InProgressOrderDTO:
+  given DTO[InProgressOrder, InProgressOrderDTO] = interCaseClassDTO
+  private given DTO[InProgressOrderLine.Complete, CompleteOrderLineDTO] = interCaseClassDTO
+  private given DTO[InProgressOrderLine.Incomplete, IncompleteOrderLineDTO] = interCaseClassDTO
+  private given DTO[InProgressOrderLine, InProgressOrderLineDTO] = new DTO:
+    override def elemToDto(e: InProgressOrderLine): InProgressOrderLineDTO = e match
+      case c: InProgressOrderLine.Complete => InProgressOrderLineDTO("complete", Some(c.toDTO), None)
+      case i: InProgressOrderLine.Incomplete => InProgressOrderLineDTO("incomplete", None, Some(i.toDTO))
+    override def dtoToElem(dto: InProgressOrderLineDTO): Either[String, InProgressOrderLine] = dto.tag match
+      case "complete" =>
+        dto.completeDTO match
+          case Some(dto) => dto.toDomain[InProgressOrderLine.Complete]
+          case None => "Found tag 'complete' but complete data is missing".asLeft[InProgressOrderLine]
+      case "incomplete" =>
+        dto.incompleteDTO match
+          case Some(dto) => dto.toDomain[InProgressOrderLine.Incomplete]
+          case None => "Found tag 'incomplete' but incomplete data is missing".asLeft[InProgressOrderLine]
+      case s => s"Unknown tag: $s".asLeft[InProgressOrderLine]
 
 final case class ProductPalletizedDTO(product: ProductDTO, quantity: Int)
 object ProductPalletizedDTO:
