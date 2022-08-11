567d085cbdab0fc7646a8722e8e37de618ead485
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-11 17:14:37 +0200

    fix: in handleOrderReceived use order instead of ordered product

--- production-planning/src/main/scala/dev/atedeg/mdm/productionplanning/api/repositories/Repositories.scala
+++ production-planning/src/main/scala/dev/atedeg/mdm/productionplanning/api/repositories/Repositories.scala
@@ -3,10 +3,10 @@ package dev.atedeg.mdm.productionplanning.api.repositories
 import cats.Monad
 import cats.effect.LiftIO
 
-import dev.atedeg.mdm.productionplanning.dto.{ CheeseTypeRipeningDaysDTO, OrderDTO, OrderedProductDTO }
+import dev.atedeg.mdm.productionplanning.dto.{ CheeseTypeRipeningDaysDTO, OrderDTO }
 
 trait ReceivedOrderRepository:
-  def saveNewOrder[M[_]: Monad: LiftIO](orderedProducts: List[OrderedProductDTO]): M[Unit]
+  def saveNewOrder[M[_]: Monad: LiftIO](order: OrderDTO): M[Unit]
   def getOrders[M[_]: Monad: LiftIO]: M[List[OrderDTO]]
 
 trait RipeningDaysRepository:
