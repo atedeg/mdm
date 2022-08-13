ae46d79accf0d1bb8619ce6867c1cce8fada10fd
ndido98 <ndido98@gmail.com>
2022-08-13 18:46:00 +0200

    refactor(docs): change image source via css

--- docs/_assets/js/imageDark.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('html')
-    .classList.contains("theme-dark")
-    ? $('#clean-arch').append('<img alt="Clean Architecture" src="../images/cleanArchitecture_dark.svg"/>')
-    : $('#clean-arch').append('<img alt="Clean Architecture" src="../images/cleanArchitecture.svg"/>')
