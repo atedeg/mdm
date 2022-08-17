4324c8956f079ebe2a5a9698a0a5040567b6aafc
ndido98 <ndido98@gmail.com>
2022-08-17 18:15:13 +0200

    fix(pricing): move .euroCents extension method from positive to non-negative number

--- pricing/src/main/scala/dev/atedeg/mdm/pricing/utils/TypesOps.scala
+++ pricing/src/main/scala/dev/atedeg/mdm/pricing/utils/TypesOps.scala
@@ -4,8 +4,9 @@ import dev.atedeg.mdm.pricing.*
 import dev.atedeg.mdm.products.Product
 import dev.atedeg.mdm.utils.*
 
+extension (n: NonNegativeNumber) def euroCents: PriceInEuroCents = PriceInEuroCents(n)
+
 extension (n: PositiveNumber)
-  def euroCents: PriceInEuroCents = PriceInEuroCents(n)
   def of(p: Product): IncomingOrderLine = IncomingOrderLine(Quantity(n), p)
   def quantity: Quantity = Quantity(n)
   def threshold: ThresholdQuantity = ThresholdQuantity(n)
