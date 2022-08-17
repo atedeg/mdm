a062b5f6ed06ecbe021a9e9cb03ac945bee35e67
ndido98 <ndido98@gmail.com>
2022-08-17 14:51:59 +0200

    fix(pricing): address review comments

--- pricing/src/main/scala/dev/atedeg/mdm/pricing/Actions.scala
+++ pricing/src/main/scala/dev/atedeg/mdm/pricing/Actions.scala
@@ -2,36 +2,53 @@ package dev.atedeg.mdm.pricing
 
 import java.time.LocalDateTime
 
+import cats.data.NonEmptyList
 import cats.syntax.all.*
 
+import dev.atedeg.mdm.pricing.utils.{ percent as _, * }
 import dev.atedeg.mdm.utils.*
 import dev.atedeg.mdm.utils.given
 
 @SuppressWarnings(Array("org.wartremover.warts.ListAppend"))
 def priceOrderLine(
     priceList: PriceList,
-    promotionsList: PromotionsList,
+    clientPromotions: List[Promotion],
     today: LocalDateTime,
-)(client: Client, orderLine: IncomingOrderLine): PriceInEuroCents =
+)(orderLine: IncomingOrderLine): PriceInEuroCents =
   val (quantity, product) = (orderLine.quantity, orderLine.product)
-  val basePrice = priceList.priceList(product)
-  val clientPromotions = promotionsList.promotions.get(client) match
-    case Some(promotions) => promotions.toList
-    case None => List.empty[Promotion]
+  val baseUnitPrice = priceList.priceList(product)
   val activePromotions = clientPromotions.filter(_.expiryDate.isAfter(today))
   val lines = activePromotions.flatMap(_.lines.toList).filter(_.product === product)
-  // Accumulate the fixed discounts
-  val fixedDiscount = lines.collect { case PromotionLine.Fixed(_, discount) =>
-    (100.0 - discount.n.value) / 100.0
-  }.foldRight(1.0)(_ * _)
-  // Get how many products fall into which thresholds
-  val (thresholds, discounts) = lines.collect {
+  // Get the fixed discounts
+  val fixedDiscounts = lines.collect { case p @ PromotionLine.Fixed(_, _) => p }
+  // Get the threshold discounts
+  val thresholdDiscounts = lines.collect { case p @ PromotionLine.Threshold(_, _, _) => p }
+  val basePrice = PriceInEuroCents(baseUnitPrice.n * quantity.n)
+  basePrice
+    |> applyFixedDiscount(fixedDiscounts)
+    |> applyThresholdDiscount(thresholdDiscounts, quantity)
+
+private def applyFixedDiscount(promotionLines: List[PromotionLine.Fixed])(
+    basePrice: PriceInEuroCents,
+): PriceInEuroCents =
+  val maybeDiscount = promotionLines.map(_.discount).reduceOption(_ * _)
+  maybeDiscount match
+    case Some(discount) => basePrice withDiscount discount
+    case None => basePrice
+
+private def applyThresholdDiscount(promotionLines: List[PromotionLine.Threshold], quantity: Quantity)(
+    basePrice: PriceInEuroCents,
+): PriceInEuroCents =
+  val filteredLines = promotionLines.collect {
     case PromotionLine.Threshold(_, threshold, discount) if threshold.n.value < quantity.n.value =>
-      (threshold.n.value, (100.0 - discount.n.value) / 100.0)
-  }.sorted.unzip
-  val chunks = 0 +: thresholds :+ quantity.n.value
-  val chunksSizes = chunks.drop(1).zip(chunks).map(_ - _)
-  val chunksDiscounts = 1.0 +: discounts
-  val thresholdDiscount = chunksSizes.zip(chunksDiscounts).foldRight(0.0)((elem, acc) => (acc + elem._1) * elem._2)
-  val orderLinePrice = basePrice.n.value * fixedDiscount * thresholdDiscount
-  PriceInEuroCents(coerce(math.ceil(orderLinePrice).toInt))
+      (threshold, discount)
+  }.sortBy(_._1.n.value)
+  if filteredLines.isEmpty
+  then basePrice
+  else
+    val percentages = filteredLines.map(_._2.toPercentage.inverted).scan(100.0.percent)(_ * _)
+    val ranges = filteredLines.map(_._1.n.value).prepended(0).appended(quantity.n.value)
+    val distances = ranges.drop(1).zip(ranges).map(_ - _)
+    val multiplier = 1.0 - percentages.zip(distances).map(_.value * _).sum / quantity.n.value
+    val discount = DiscountPercentage(coerce(multiplier))
+    basePrice withDiscount discount
--- utils/src/main/scala/dev/atedeg/mdm/utils/Utils.scala
+++ utils/src/main/scala/dev/atedeg/mdm/utils/Utils.scala
@@ -3,3 +3,5 @@ package dev.atedeg.mdm.utils
 import cats.kernel.Order
 
 def max[A: Order](a1: A, a2: A): A = Order[A].max(a1, a2)
+
+extension [A](a: A) def |>[B](f: A => B): B = f(a)
