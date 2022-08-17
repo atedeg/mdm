a16091c86f331aec689064f11662aefb4a1d8f6f
ndido98 <ndido98@gmail.com>
2022-08-17 17:16:21 +0200

    chore(pricing): add promotion repository

--- pricing/src/main/scala/dev/atedeg/mdm/pricing/api/repositories/Repositories.scala
+++ pricing/src/main/scala/dev/atedeg/mdm/pricing/api/repositories/Repositories.scala
@@ -8,3 +8,6 @@ import dev.atedeg.mdm.pricing.dto.*
 
 trait PriceListRepository:
   def read[M[_]: Monad: LiftIO]: M[PriceListDTO]
+
+trait PromotionsRepository:
+  def readByClientID[M[_]: Monad: LiftIO](clientID: String): M[List[PromotionDTO]]
