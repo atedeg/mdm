437a9cdf764b18f13d31116238a471449daf0571
ndido98 <ndido98@gmail.com>
2022-08-17 17:11:04 +0200

    chore(pricing): add price list repository
    Former-commit-id: 8500f9fa29d6122d720d644cb2c6de9e254b744d

---
+++ pricing/src/main/scala/dev/atedeg/mdm/pricing/api/repositories/Repositories.scala
@@ -0,0 +1,10 @@
+package dev.atedeg.mdm.pricing.api.repositories
+
+import cats.Monad
+import cats.effect.LiftIO
+import cats.syntax.all.*
+
+import dev.atedeg.mdm.pricing.dto.*
+
+trait PriceListRepository:
+  def read[M[_]: Monad: LiftIO]: M[PriceListDTO]
