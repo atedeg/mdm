1387e24912bf6a65e2525ca292f6b42da6e61649
ndido98 <ndido98@gmail.com>
2022-08-18 10:46:31 +0200

    feat(pricing): add http endpoint for pricing an order line

---
+++ pricing/src/main/scala/dev/atedeg/mdm/pricing/api/endpoints/Endpoints.scala
@@ -0,0 +1,36 @@
+package dev.atedeg.mdm.pricing.api.endpoints
+
+import cats.effect.IO
+import io.circe.generic.auto.*
+import org.http4s.HttpRoutes
+import sttp.tapir.*
+import sttp.tapir.generic.auto.*
+import sttp.tapir.json.circe.jsonBody
+import sttp.tapir.server.http4s.Http4sServerInterpreter
+
+import dev.atedeg.mdm.pricing.api.*
+import dev.atedeg.mdm.pricing.api.repositories.*
+import dev.atedeg.mdm.pricing.dto.*
+import dev.atedeg.mdm.products.dto.*
+import dev.atedeg.mdm.utils.monads.*
+
+object PricingEndpoints:
+  @SuppressWarnings(Array("org.wartremover.warts.Any"))
+  val priceOrderLineEndpoint: PublicEndpoint[(String, Int, String, Int), String, PriceInEuroCentsDTO, Any] =
+    endpoint.get
+      .in("price")
+      .in(query[String]("clientID").description("The client ID"))
+      .in(query[Int]("quantity").description("The quantity of the product to be priced"))
+      .in(query[String]("cheeseType").description("The type of cheese in the order line to be priced"))
+      .in(query[Int]("weight").description("The weight of the cheese in the order line to be priced"))
+      .out(jsonBody[PriceInEuroCentsDTO].description("The price of the order line"))
+      .errorOut(stringBody)
+
+  val priceOrderLineRoute: HttpRoutes[IO] = Http4sServerInterpreter[IO]().toRoutes(
+    priceOrderLineEndpoint.serverLogic { (clientID, quantity, cheeseType, weight) =>
+      val product = ProductDTO(cheeseType, weight)
+      val orderLine = IncomingOrderLineDTO(quantity, product)
+      val action: ServerAction[Configuration, String, PriceInEuroCentsDTO] = priceOrderLineHandler(clientID, orderLine)
+      action.value.run(Configuration(PriceListRepositoryDB("foo"), PromotionsRepositoryDB("bar")))
+    },
+  )
