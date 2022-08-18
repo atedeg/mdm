c06d818874b44b32dccc444da0441e9a10c4fe1f
ndido98 <ndido98@gmail.com>
2022-08-18 10:45:15 +0200

    chore(pricing): add mock repositories
    Former-commit-id: a77d99b060cd4f14bad18ada4fe1160e3ec026b1

--- pricing/src/main/scala/dev/atedeg/mdm/pricing/api/repositories/Repositories.scala
+++ pricing/src/main/scala/dev/atedeg/mdm/pricing/api/repositories/Repositories.scala
@@ -11,3 +11,9 @@ trait PriceListRepository:
 
 trait PromotionsRepository:
   def readByClientID[M[_]: Monad: LiftIO](clientID: String): M[List[PromotionDTO]]
+
+final case class PriceListRepositoryDB(connectionString: String) extends PriceListRepository:
+  override def read[M[_]: Monad: LiftIO]: M[PriceListDTO] = ???
+
+final case class PromotionsRepositoryDB(connectionString: String) extends PromotionsRepository:
+  override def readByClientID[M[_]: Monad: LiftIO](clientID: String): M[List[PromotionDTO]] = ???
